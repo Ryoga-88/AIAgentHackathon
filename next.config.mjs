7a618e1e4417f39bfd59f217b12eb1bb6776fcba
Ryoga-88 <hryoga8723@gmail.com>
2025-06-13 08:47:36 +0900

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // ビルドの最適化
+  swcMinify: true,
+  // 不要なファイルの除外
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
